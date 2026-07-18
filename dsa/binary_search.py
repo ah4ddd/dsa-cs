84db79dd5a9289b07c60af01c82133f33c0a291c
Ahad <mohdahad60080@gmail.com>
2026-07-18 16:22:28 +0530

    with explanation

--- dsa/binary_search.py
+++ dsa/binary_search.py
@@ -1,17 +1,45 @@
 def binary_search(nums, target):
+# The variables left, right, and mid always store INDEXES, not the actual values from the array.
     left = 0
-    right = len(nums)-1
+    right = len(nums) - 1
+
+    print("Array :", nums)
+    print("Target:", target)
+    print()
 
     while left <= right:
-        mid = (left + right)//2
+
+        print("-----------------------------")
+        print("left :", left)
+        print("right:", right)
+
+        mid = (left + right) // 2
+
+        print("mid  :", mid)
+        print("Value at mid:", nums[mid])
 
         if nums[mid] == target:
+            print("Target Found!")
             return mid
 
         elif nums[mid] < target:
+            print("Target is BIGGER.")
+            print("Move left pointer.")
+
             left = mid + 1
 
         else:
+            print("Target is SMALLER.")
+            print("Move right pointer.")
+
             right = mid - 1
 
+    print("Target not found.")
     return -1
+
+
+nums = [2, 5, 7, 9, 13, 18, 22, 30]
+
+result = binary_search(nums, 18)
+
+print("\nReturned Index:", result)
