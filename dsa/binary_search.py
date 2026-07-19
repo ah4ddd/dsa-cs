a7e52211b75006ee4405a2eca28053556ca0f853
Ahad <mohdahad60080@gmail.com>
2026-07-19 14:40:42 +0530

    i understand it now

--- dsa/binary_search.py
+++ dsa/binary_search.py
@@ -1,17 +1,23 @@
-nums = [2, 5, 7, 9, 13, 18, 22, 30, 45, 60, 78, 85, 100]
+nums=[2, 5, 7, 9, 13, 18, 22, 30, 45, 60, 78, 85, 100]
 
 def binary_search(nums, target):
     left = 0
     right = len(nums) - 1
 
     while left <= right:
-        mid = (left+right)//2
+        mid = (left + right) // 2
+
         if nums[mid] == target:
             return mid
-        elif nums[mid] < target:
-            left = mid+1
-        else:
-            right = mid-1
 
-result = binary_search(nums, 2)
+        elif nums[mid] < target:
+            left = mid + 1
+
+        else:
+            right = mid - 1
+
+    return -1
+
+result = binary_search(nums, 85)
 print("\nReturned Index:", result)
+
