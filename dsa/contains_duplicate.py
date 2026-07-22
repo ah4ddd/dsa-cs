520c1f1e0b72d8937b2df7ade29bc4b2c7540de4
Ahad <mohdahad60080@gmail.com>
2026-07-22 12:16:09 +0530

    duplicate

--- dsa/contains_duplicate.py
+++ dsa/contains_duplicate.py
@@ -56,3 +56,12 @@ def hasDuplicate(nums) -> bool:
 print(hasDuplicate(array2))
 print(hasDuplicate(array1))
 print(hasDuplicate(array5))
+
+
+def Duplicate(nums) -> bool:
+        seen = {}
+        for num in nums:
+            if num in seen and seen[num] >= 1:
+                return True
+            seen[num] = seen.get(num, 0) + 1
+        return False
