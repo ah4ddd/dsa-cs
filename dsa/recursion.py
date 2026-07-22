c18a2223faa7e7d07e622317b24c82c455518f1e
Ahad <mohdahad60080@gmail.com>
2026-07-22 20:12:23 +0530

    actual use

--- dsa/recursion.py
+++ dsa/recursion.py
@@ -1,5 +1,13 @@
-def hello():
-    print("hello Ahad")
-    hello()
+# Every recursive function must know when to stop.
+# This stopping point is called the Base Case
+# Without a base case, the function never ends.
 
-hello()
+def count(n):
+    if n == 6:
+        return
+
+    print(n)
+
+    count(n+1)
+
+count(1)
