d8b483dbc57564a9d84371c77778e0e6420914ee
Ahad <mohdahad60080@gmail.com>
2026-07-26 17:18:46 +0530

    optimized

--- challenges-probs/c10.py
+++ challenges-probs/c10.py
@@ -13,23 +13,18 @@ words = [
 
 def get_first_unique_word(words):
     count = {}
-    unique = []
     for w in words:
         if w in count:
             count[w] += 1
         else:
             count[w] = 1
 
-    for c in count:
-        if count[c] == 1:
-            unique.append(c)
-
-    if unique:
-        return unique[0]
+    for w in words:
+        if count[w] == 1:
+            return w
 
     return None
 
 
 result = get_first_unique_word(words)
-
 print(f"unique word: {result}")
