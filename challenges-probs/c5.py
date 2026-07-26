fdda2f171a586da7680165824bfc5fee206a934b
Ahad <mohdahad60080@gmail.com>
2026-07-26 14:09:21 +0530

    patterns

--- challenges-probs/c5.py
+++ challenges-probs/c5.py
@@ -3,6 +3,7 @@ def get_largest(nums):
         return None
 
     largest = nums[0]
+  # largest = float("-inf") < works too
 
     for n in nums[1:]:
         if n > largest:
@@ -13,3 +14,29 @@ def get_largest(nums):
 numbers = [-12, -7, -3, -25]
 
 get_largest(numbers)
+
+
+"""
+Pattern #1 — Counting
+count = 0
+
+for item in items:
+    if condition:
+        count += 1
+
+
+Pattern #2 — Categorizing
+evens = []
+odds = []
+
+for item in items:
+    ...
+
+
+Pattern #3 — Best-so-far ⭐
+best = items[0]
+
+for item in items[1:]:
+    if item is better:
+        best = item
+"""
