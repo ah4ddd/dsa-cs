469abfa3cd5e564f5513ed59e98d99c08495d53d
Ahad <mohdahad60080@gmail.com>
2026-07-26 14:23:53 +0530

    not need []

--- challenges-probs/c6.py
+++ challenges-probs/c6.py
@@ -3,12 +3,10 @@
 numbers = [7, 12, 5, 18, 21, 30, 9, 4]
 
 def sum_even(nums):
-    even = []
     sum = 0
     for n in nums:
         if n % 2 == 0:
             sum += n
-            even.append(n)
 
     print(f"sum of even numbers: {sum}")
 
