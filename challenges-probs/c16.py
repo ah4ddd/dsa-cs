d7693efba53fda9a662ed84725dfe8c361930d71
Ahad <mohdahad60080@gmail.com>
2026-07-27 21:10:40 +0530

    c16 palindrome done

---
+++ challenges-probs/c16.py
@@ -0,0 +1,20 @@
+# Challenge 16 — Is the List a Palindrome?
+
+numbers = [1, 2, 3, 2, 1]
+
+numbers2 = [1, 2, 3, 4, 5]
+
+def is_pelindrome(nums):
+    for i in range(len(nums)//2):
+        if nums[i] != nums[-(i+1)]:
+            return False
+    return True
+
+result = is_pelindrome(numbers)
+result2 = is_pelindrome(numbers2)
+
+print(result)
+print(result2)
+
+
+
