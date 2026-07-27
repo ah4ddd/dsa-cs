aa1b99b70d75f72c82440b2870aaa0a7143e11e4
Ahad <mohdahad60080@gmail.com>
2026-07-27 21:15:24 +0530

    typo

--- challenges-probs/c16.py
+++ challenges-probs/c16.py
@@ -4,17 +4,14 @@ numbers = [1, 2, 3, 2, 1]
 
 numbers2 = [1, 2, 3, 4, 5]
 
-def is_pelindrome(nums):
+def is_palindrome(nums):
     for i in range(len(nums)//2):
         if nums[i] != nums[-(i+1)]:
             return False
     return True
 
-result = is_pelindrome(numbers)
-result2 = is_pelindrome(numbers2)
+result = is_palindrome(numbers)
+result2 = is_palindrome(numbers2)
 
 print(result)
 print(result2)
-
-
-
