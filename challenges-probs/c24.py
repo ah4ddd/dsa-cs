471b26575ad58e24b8f3da909900e9207330c2be
Ahad <mohdahad60080@gmail.com>
2026-07-30 22:31:46 +0530

    documented

--- challenges-probs/c24.py
+++ challenges-probs/c24.py
@@ -4,7 +4,9 @@ prices = [5, 8, 1, 4, 6, 3]
 
 
 def max_profit(prices):
+    # Keep track of the cheapest price seen so far.
     lowest = prices[0]
+    # Update the best profit found.
     best = 0
 
     buy = prices[0]
@@ -13,7 +15,7 @@ def max_profit(prices):
     for p in prices:
         if p < lowest:
             lowest = p
-
+        # Profit if we sold today.
         todays_profit = p - lowest
 
         if todays_profit > best:
@@ -52,3 +54,23 @@ def maxProfit(prices):
 profit = maxProfit(prices)
 
 print(profit)
+
+
+"""
+Pattern:
+    running minimum
+
+    +
+
+    running maximum (or best answer)
+
+
+Whenever you process data in one pass, ask:
+
+    What is the best thing I've seen so far?
+    What is the worst thing I've seen so far?
+    What is the smallest value so far?
+    What is the largest value so far?
+
+That thinking appears everywhere.
+"""
