9dc23dad06f053e236753f2062c8cbda7f53ecce
Ahad <mohdahad60080@gmail.com>
2026-08-18 10:14:31 +0530

    nums[:]

--- challenges-probs/c37.py
+++ challenges-probs/c37.py
@@ -5,8 +5,8 @@ nums = [0,1,2,2,3,0,4,2]
 val = 2
 
 def removeElements(nums, val):
-    new = [n for n in nums if n!=val]
-    return new
+    nums[:] = [n for n in nums if n!=val]
+    return nums
 
 r = removeElements(nums, val)
 
