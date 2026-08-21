671b5f769bca65e41f870d639aa3ec0c1bd02dc8
Ahad <mohdahad60080@gmail.com>
2026-08-21 21:41:25 +0530

    Time: 1 ms (56.54%), Space: 20.5 MB (42.72%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,11 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # Points to the next position where a unique number should be written.
         unique_pos = 1
         
-        for cur in range(1, len(nums)):
-            if nums[cur] != nums[cur - 1]:
-                nums[unique_pos] = nums[cur]
+        for c in range(1, len(nums)):
+            if nums[c] != nums[c-1]:
+                nums[unique_pos] = nums[c]
                 unique_pos += 1
-                
+
         return unique_pos
+
+
