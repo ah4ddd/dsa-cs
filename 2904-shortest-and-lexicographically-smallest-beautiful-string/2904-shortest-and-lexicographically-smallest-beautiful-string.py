7bee4e80ab00507a0b050b12fa8619598ecca0d1
Ahad <mohdahad60080@gmail.com>
2026-08-26 13:57:29 +0530

    Time: 37 ms (5.86%), Space: 19.3 MB (30.63%) - LeetHub

---
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
@@ -0,0 +1,3 @@
+class Solution:
+    def shortestBeautifulSubstring(self,s,k):
+        return min((s[i:j] for i in range(len(s)) for j in range(i+1,len(s)+1) if s[i:j].count('1')==k),key=lambda x:(len(x),x),default="")
